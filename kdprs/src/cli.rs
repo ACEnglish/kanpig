f152bf0167287167e031297675fe141cd70af7b4
Adam English <ACEnglish@gmail.com>
2024-02-24 17:38:11 -0500

    adding cli docs

--- kdprs/src/cli.rs
+++ kdprs/src/cli.rs
@@ -14,57 +14,74 @@ pub struct ArgParser {
 
 #[derive(clap::Args, Debug)]
 pub struct IOParams {
+    /// VCF to genotype
     #[arg(short, long)]
     pub input: std::path::PathBuf,
 
+    /// Reads to genotype
     #[arg(short, long)]
     pub bam: std::path::PathBuf,
 
+    /// Reference bam is aligned to 
     #[arg(short = 'f', long)]
     pub reference: std::path::PathBuf,
 
+    /// Output vcf (unsorted)
     #[arg(short, long)]
     pub out: std::path::PathBuf,
 
+    /// Regions to analyze
     #[arg(long)]
     pub bed: Option<std::path::PathBuf>,
 
+    /// Sample to apply genotypes to, default first column
     #[arg(long)]
     pub sample: Option<String>,
 }
 
 #[derive(clap::Args, Debug, Clone)]
 pub struct KDParams {
+    /// Kmer size for featurization
     #[arg(long, default_value_t = 4)]
     pub kmer: u8,
 
+    /// Minimum distance between variants to create independent graphs
+    #[arg(long, default_value_t = 100)]
+    pub chunksize: u64,
+
+    /// Only analyze reads with PASS FILTER
     #[arg(long, default_value_t = false)]
     pub passonly: bool,
 
+    /// Minimum size of variant to analyze
     #[arg(long, default_value_t = 20)]
     pub sizemin: u64,
 
     // change this to 10k? also filtered variants are just plain gone...
+    /// Maximum size of variant to analyze
     #[arg(long, default_value_t = 50000)]
     pub sizemax: u64,
 
+    /// Maximum number of paths in a graph to traverse
     #[arg(long, default_value_t = 1000)]
     pub maxpaths: usize,
 
+    /// Minimum cosine similarity for paths
     #[arg(long, default_value_t = 0.90)]
     pub cossim: f32,
 
+    /// Minimum size similarity for paths
     #[arg(long, default_value_t = 0.90)]
     pub pctsize: f32,
 
+    /// Size threshold above which weighted cosine similarity is used
     #[arg(long, default_value_t = 2000)]
     pub wcoslen: usize,
 
+
+    /// Number of attempts through the graph (experimental)
     #[arg(long, default_value_t = 5)]
     pub n_tries: usize,
-
-    #[arg(long, default_value_t = 100)]
-    pub chunksize: u64,
 }
 
 impl ArgParser {
