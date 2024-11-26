abdfd8159461de5a41ddbc8ec9e86f3ce8c73841
Adam English <ACEnglish@gmail.com>
2024-11-26 18:20:28 -0500

    minor clean

--- src/kplib/pileup_index.rs
+++ src/kplib/pileup_index.rs
@@ -109,14 +109,13 @@ pub fn plup_main(args: PlupArgs) {
         for record in bam.records().filter_map(|result| {
             match result {
                 Ok(record) => {
-                    // Filter out records that are empty or have the unwanted flags
-                    if !record.seq().is_empty() && (record.flags() & args.mapflag) == 0 && record.mapq() >= args.mapq {
-                        Some(record) // Only keep the valid records
+                    if record.seq().is_empty() || record.mapq() < args.mapq || (record.flags() & args.mapflag) != 0 {
+                        None
                     } else {
-                        None // Filter out invalid records
+                        Some(record)
                     }
                 }
-                Err(_) => None, // Filter out any errors
+                Err(_) => None,
             }
         }) {
             buffer.push(record);
