36b960bc6dba3bcecb23200b222a0301ec5eaace
Adam English <ACEnglish@gmail.com>
2024-12-25 10:30:42 -0500

    clippy fix

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ mod kplib;
 pub use self::{
     kplib::brute_force_find_path, kplib::build_region_tree, kplib::seq_to_kmer, kplib::BamParser,
     kplib::BedParser, kplib::Cli, kplib::Commands, kplib::GTArgs, kplib::Haplotype,
-    kplib::IOParams, kplib::KDParams, kplib::KanpigParams, kplib::KdpVcf, kplib::PathScore,
-    kplib::Ploidy, kplib::PloidyRegions, kplib::PlupArgs, kplib::PlupParser, kplib::ReadParser,
-    kplib::ReadPileup, kplib::Regions, kplib::Svtype, kplib::VarNode, kplib::Variants,
-    kplib::VcfChunker, kplib::VcfWriter,
+    kplib::IOParams, kplib::KDParams, kplib::KanpigParams, kplib::KdpVcf, kplib::PasteArgs,
+    kplib::PathScore, kplib::Ploidy, kplib::PloidyRegions, kplib::PlupArgs, kplib::PlupParser,
+    kplib::ReadParser, kplib::ReadPileup, kplib::Regions, kplib::Svtype, kplib::VarNode,
+    kplib::Variants, kplib::VcfChunker, kplib::VcfWriter,
 };
