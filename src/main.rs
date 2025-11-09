e040f5bb9386f9958011b8cbc6c28d2a55d84f55
Adam English <ACEnglish@gmail.com>
2025-11-09 11:03:22 -0500

    update logging
    better format, local time

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
+use chrono::Local;
 use clap::Parser;
 use kanpig::commands::{Commands, KanpigCommand};
+use log::LevelFilter;
+use pretty_env_logger::env_logger::fmt::{Color, Formatter, WriteStyle};
+use std::io::Write;
 
 /// Entrypoint for the commands
 #[derive(Parser, Clone, Debug)]
@@ -18,13 +22,37 @@ pub struct Cli {
 
 fn setup_logging(args: &impl KanpigCommand) {
     let level = if args.debug() {
-        log::LevelFilter::Debug
+        LevelFilter::Debug
     } else {
-        log::LevelFilter::Info
+        LevelFilter::Info
     };
 
     pretty_env_logger::formatted_timed_builder()
         .filter_level(level)
+        .format(|buf: &mut Formatter, record| {
+            let ts = Local::now().format("%Y-%m-%dT%H:%M:%S");
+            let mut level_style = buf.style();
+
+            match record.level() {
+                log::Level::Error => level_style.set_color(Color::Red).set_bold(true),
+                log::Level::Warn => level_style.set_color(Color::Yellow).set_bold(true),
+                log::Level::Info => level_style.set_color(Color::Green),
+                log::Level::Debug => level_style.set_color(Color::Blue),
+                log::Level::Trace => level_style.set_color(Color::Cyan),
+            };
+
+            let target = record.module_path().unwrap_or_default();
+
+            writeln!(
+                buf,
+                " {} {:<5} {:<25} > {}",
+                ts,
+                level_style.value(record.level()),
+                target,
+                record.args()
+            )
+        })
+        .write_style(WriteStyle::Auto)
         .init();
 
     info!("params: {:#?}", args);
