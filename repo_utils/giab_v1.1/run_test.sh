f93f3d0ea023750a44a7389dec1b64bf03578f57
Adam English <ACEnglish@gmail.com>
2026-01-23 18:27:43 -0500

    progress

--- repo_utils/giab_v1.1/run_test.sh
+++ repo_utils/giab_v1.1/run_test.sh
@@ -1,6 +1,5 @@
-# Remember that these tests are run from the repo root directory
 mkdir -p ${OD}/giab_v1.1/
-run giab_default cargo run -- \
+$kanpig \
     gt \
     --sample kanpig \
     --reads ${TESTSRC}/giab_v1.1/reads.bam \
@@ -9,24 +8,10 @@ run giab_default cargo run -- \
     --reference ${REF} \
     -o ${OD}/giab_v1.1/default_output.vcf
 
-
-if [ $giab_default ]; then
-    assert_exit_code 0
-
-    bcftools sort ${OD}/giab_v1.1/default_output.vcf -O z -o ${OD}/giab_v1.1/default_output.vcf.gz
-    tabix ${OD}/giab_v1.1/default_output.vcf.gz
-    bcftools merge -m none -O z \
-            ${TESTSRC}/giab_v1.1/baseline.vcf.gz \
-            ${OD}/giab_v1.1/default_output.vcf.gz \
-            -o ${OD}/giab_v1.1/merged.vcf.gz
-    tabix ${OD}/giab_v1.1/merged.vcf.gz
-
-    # This needs some asserts
-    # I wouldn't mind keeping commit to perf records, either
-    # Wrap this in an actual test
-    python ${TESTSRC}/giab_v1.1/calc_perf.py ${OD}/giab_v1.1/merged.vcf.gz ${TESTSRC}/giab_v1.1/include.bed
-    assert_exit_code 0
-fi
-
-# Be sure to exit back out
-cd - 
+bcftools sort ${OD}/giab_v1.1/default_output.vcf -O z -o ${OD}/giab_v1.1/default_output.vcf.gz
+tabix ${OD}/giab_v1.1/default_output.vcf.gz
+bcftools merge -m none -O z \
+        ${TESTSRC}/giab_v1.1/baseline.vcf.gz \
+        ${OD}/giab_v1.1/default_output.vcf.gz \
+        -o ${OD}/giab_v1.1/merged.vcf.gz
+tabix ${OD}/giab_v1.1/merged.vcf.gz
--- repo_utils/kanpig_ssshtests.sh
+++ repo_utils/kanpig_ssshtests.sh
@@ -1,15 +1,19 @@
 # Functional tests
-test -e ssshtest || curl -O https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
-source ssshtest
 
 # Run from truvari's base directory
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../
 
+GITHASH=$(git rev-parse --short HEAD)
+
 OD=test_results
 TESTSRC=repo_utils/
 REF=$TESTSRC/GRCh38_chr20.fa
-
 kanpig="cargo run -- "
+
+LOGDIR=${TESTSRC}/history/${GITHASH}
+mkdir -p ${LOGDIR}
+exec > >(tee -a ${LOGDIR}/main.out)
+exec 2> >(tee -a ${LOGDIR}/main.err >&2)
 
 # Reset test results
 rm -rf $OD
@@ -18,3 +22,5 @@ mkdir -p $OD
 source $TESTSRC/giab_v1.1/run_test.sh
 #source $TESTSRC/plat/run_test.sh
 #source $TESTSRC/mims/run_test.sh
+
+python ${TESTSRC}/giab_v1.1/calc_perf.py ${OD}/giab_v1.1/merged.vcf.gz ${TESTSRC}/giab_v1.1/include.bed
